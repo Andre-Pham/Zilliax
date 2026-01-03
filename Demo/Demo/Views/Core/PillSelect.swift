4f0663d4132f0403878b94658d52ce69b8661b32
Andre Pham <andrepham33@gmail.com>
2026-01-03 14:26:27 +1100

    init

---
+++ Demo/Demo/Views/Core/PillSelect.swift
@@ -0,0 +1,15 @@
+//
+//  PillSelect.swift
+//  https://github.com/Andre-Pham/Zilliax
+//
+//  Created by Andre Pham.
+//
+
+public class PillSelect: View {
+    public override func setup() {
+        super.setup()
+
+        self.setBackgroundColor(to: Colors.fillBackground)
+            .setCornerRadius(to: 16)
+    }
+}
